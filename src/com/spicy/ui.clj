53ff8d4d5fd09eeedcf5c87b271ba95ba6050fbd
Zac Jones <zacjones93@gmail.com>
2023-07-21 14:54:08 -0600

    feat: add favicons to site

--- src/com/spicy/ui.clj
+++ src/com/spicy/ui.clj
@@ -34,6 +34,13 @@
                                       [:link {:rel "stylesheet" :href (css-path)}]
                                       [:script {:src "/js/alpinejs_3_12_1.js" :defer true}]
                                       [:script {:src "/js/htmx_1_9_2.js" :defer true}]
+                                      [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/img/apple-touch-icon.png"}]
+                                      [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/img/favicon-32x32.png"}]
+                                      [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/img/favicon-16x16.png"}]
+                                      [:link {:rel "manifest" :href "/img/site.webmanifest"}]
+                                      [:link {:rel "mask-icon" :href "/img/safari-pinned-tab.svg" :color "#0c0001"}]
+                                      [:meta {:name "msapplication-TileColor" :content "#ffeef0"}]
+                                      [:meta {:name "theme-color" :content "#ffffff"}]
                                       (when recaptcha
                                         [:script {:src "https://www.google.com/recaptcha/api.js"
                                                   :async "async" :defer "defer"}])]
