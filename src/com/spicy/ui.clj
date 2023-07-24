e27132225cc1298c521d96edbe081053505033df
Zac Jones <zacjones93@gmail.com>
2023-07-24 10:00:34 -0600

    feat: add mobile nav menu

--- src/com/spicy/ui.clj
+++ src/com/spicy/ui.clj
@@ -52,24 +52,71 @@
   [{:keys [session] :as ctx} & body]
   (base
     ctx
-    [:.relative.h-full
+    [:.relative.h-full.p-4
      [:div {:class "absolute h-full -z-10 overflow-visible inset-0 bg-[url(/img/grid.svg)] bg-center "}]
-     [:.p-3.mx-auto.max-w-screen-xl.w-full.flex.sm:justify-between.items-center.flex-wrap.space-y-2.justify-center
+     [:.p-3.mx-auto.max-w-screen-xl.w-full.flex.flex-col.sm:flex-row.gap-4.sm:justify-between.items-center.flex-wrap.space-y-2.justify-center
       [:a.flex.items-center.gap-2.cursor-pointer
        {:href "/"}
        [:img {:src "/img/spicywod-logo.png" :width 70 :height 70 :alt "spicy pepper"}]
        [:div {:class "font-display text-5xl w-[140px]"} "Spicy WOD"]]
       [:nav
-       [:ul.flex.list-none.list-inside.gap-3.pl-0.ml-0
-        [:li [:a.btn {:href "/app/workouts"} "Workouts"]]
-        [:li [:a.btn {:href "/app/movements"} "Movements"]]
-        [:li [:a.btn {:href "/app/results"} "Scores"]]
+       [:div {:class (str "relative text-left block sm:hidden ") :x-data "{ open: false }"}
+        [:div
+         [:button.btn.pink-focus {:type "button"
+                   :class (str "flex items-center focus:ring-0 mx-auto ")
+                   :id "menu-button"
+                   :aria-expanded "true"
+                   :aria-haspopup "true"
+                   :x-on:click "open = ! open"}
+          [:span.sr-only "Open options"]
+          "Menu" 
+          ]]
+        [:div {:class            (str "absolute -right-16 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ")
+               :role             "menu"
+               :aria-orientation "vertical"
+               :aria-labelledby  "menu-button"
+               :tabindex         "-1"
+               :x-show           "open"
+               }
+         [:div {:role "none" :class (str "py-1 bg-[#e5d6dc] rounded ")}
+          [:a {:href     "/app/workouts"
+               :class    (str "text-black font-bold block px-4 py-2 text-sm border-b ")
+               :role     "menuitem"
+               :tabindex "-1"
+               :id       "menu-item-0"} "Workouts"]
+          [:a {:href     "/app/movements"
+               :class    (str "text-black font-bold block px-4 py-2 text-sm border-b ")
+               :role     "menuitem"
+               :tabindex "-1"
+               :id       "menu-item-0"} "Movements"]
+          [:a {:href     "/app/results"
+               :class    (str "text-black font-bold block px-4 py-2 text-sm ")
+               :role     "menuitem"
+               :tabindex "-1"
+               :id       "menu-item-0"} "Scores"]
+          (when (:uid session) 
+            [:a {:href     "/app/results"
+               :class    (str "text-black font-bold block px-4 py-2 border-t text-sm  ")
+               :role     "menuitem"
+               :tabindex "-1"
+               :id       "menu-item-0"} 
+             (biff/form
+              {:action "/auth/signout"
+               :class  ""}
+              [:button {:type  "submit"
+                        :class (str "text-black font-bold block p-0  text-sm ")}
+               "Sign out"])
+             ])]]]
+       [:ul.hidden.sm:flex.list-none.list-inside.gap-4.pl-0.ml-0
+        [:li.py-3 [:a.btn {:href "/app/workouts"} "Workouts"]]
+        [:li.py-3 [:a.btn {:href "/app/movements"} "Movements"]]
+        [:li.py-3 [:a.btn {:href "/app/results"} "Scores"]]
         (when (:uid session)
-          (biff/form
+          [:li.py-1 (biff/form
             {:action "/auth/signout"
-             :class "inline"}
-            [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
-             "Sign out"]))]]]
+             :class "p-0 w-fit h-fit "}
+            [:button.rounded.border-2.border-black.bg-brand-background.p-2.text-2xl.font-bold.text-black {:type "submit"}
+             "Sign out"])])]]]
      [:.relative.sm:p-3.mx-auto.max-w-screen-xl.w-full
       (when (bound? #'csrf/*anti-forgery-token*)
         {:hx-headers (cheshire/generate-string
